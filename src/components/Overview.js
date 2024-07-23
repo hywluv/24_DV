2d5190f9a6da85acc17041516ded1ad532bc9dea
Starrism <hyw-luv@zju.edu.cn>
2024-07-23 16:06:54 +0800

    [fix] fix useless code in overview

--- src/components/Overview.js
+++ src/components/Overview.js
@@ -10,30 +10,8 @@ import DetailViewChart from "./Overview/detailchart";
 function Overview() {
     const {state, dispatch} = useContext(store);
 
-    const getOption = () => {
-        return {
-            xAxis: {
-                type: 'category',
-                data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
-            },
-            yAxis: {
-                type: 'value'
-            },
-            series: [{
-                data: [120, 200, 150, 80, 70, 110, 130],
-                type: 'bar',
-                showBackground: true,
-                backgroundStyle: {
-                    color: 'rgba(180, 180, 180, 0.2)'
-                }
-            }]
-        };
-    };
-
     return <div>
         <p>Overview</p>
-        {/*<ReactEcharts option={getOption()} />*/}
-        {/*<TotalFromCSV csvFile="/data/processed.csv" />*/}
         <TotalFromCSV csvFile="/data/processed.csv" />
     </div>
 }
