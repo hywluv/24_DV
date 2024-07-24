a685f679608be2901d12c76655be8fb40803cb91
Daiyang Wu <126930948+74666666666@users.noreply.github.com>
2024-07-25 06:43:41 +0800

    Update DetailView.js

--- src/components/DetailView.js
+++ src/components/DetailView.js
@@ -10,9 +10,6 @@ function DetailView() {
     const {state, dispatch} = useContext(store);
 
     return <div style={{width: '100%', height: '100%'}}>
-        <div className='detailview-container'>
-            <p className='title'>Detail View</p>
-        </div>
         {/*<p>{state.count}</p>*/}
         {/*<button onClick={() => dispatch({type: 'decrease'})}>minus</button>*/}
         {/*<APILoader akay='Yvjkj9EN8qfW4vDB6WoTfd01DYWWTzDW'>*/}
